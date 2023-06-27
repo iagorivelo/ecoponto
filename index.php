b7c5b0fab5b18496b1df97d0728fecc6c9d97646
Iago Augusto Rivelo Sampaio <100492685+iagorivel0@users.noreply.github.com>
2023-06-27 09:55:06 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,8 @@
 <?php
 
+incude 'Auth/Controller/AuthController';
+incude 'Ecoponto/Controller/EcopontoController';
+
 $parse_url = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 
 switch($parse_url)
