9dbbd0983c715ecf93284dc4e163fb30ffa37db4
Iago Augusto Rivelo Sampaio <100492685+iagorivel0@users.noreply.github.com>
2023-07-03 10:01:54 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,23 +1,4 @@
 <?php
 
-incude 'Auth/Controller/AuthController';
-incude 'Ecoponto/Controller/EcopontoController';
-
-$parse_url = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
-
-switch($parse_url)
-{
-    case '/':
-        AuthController::index();
-    
-    break;
-
-    case 'logout':
-        AuthController::logout();
-    
-    break;
-
-    default:
-        echo "Erro 404";
-    break;
-}
+include 'autoload.php';
+include 'rotas.php';
