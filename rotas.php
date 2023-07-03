c12ef0fe32df79091b0b4a0ae8e721bb2e055b82
Iago Augusto Rivelo Sampaio <100492685+iagorivel0@users.noreply.github.com>
2023-07-03 10:01:11 -0300

    Create rotas.php

---
+++ rotas.php
@@ -0,0 +1,23 @@
+<?php
+
+use Auth\Controller\AuthController;
+use Ecoponto\Controller\EcopontoController;
+
+$parse_url = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+
+switch($parse_url)
+{
+    case '/':
+        AuthController::index();
+    
+    break;
+
+    case 'logout':
+        AuthController::logout();
+    
+    break;
+
+    default:
+        echo "Erro 404";
+    break;
+}
